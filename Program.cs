98c2c00e7dd0057ecee11be38e00e66c69e1d03d
Rodrigo <rodrigo.portilio@outlook.com>
2021-02-06 15:20:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpLINQQueriesOperators
+{
+    class Program
+    {    
+        static string[] users = new []{ "Emily", "Jacob", "Thomas" };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a option:");
+            Console.WriteLine("1 - WhereAndOrderByClause");
+            Console.WriteLine("2 - GroupClause");
+            var option = Int32.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    WhereAndOrderByClause();
+                    break;
+                case 2:
+                    GroupClause();
+                    break;
+                default:
+                    Console.WriteLine("Option invalid!");
+                    break;
+            }
+        }
+
+        static void WhereAndOrderByClause()
+        {
+            var userQuery = 
+                from user in users
+                where user.Contains("m") // Where Clause
+                orderby user.Length ascending // Orderby Clause | Can arrange the result in ascending or descending order
+                select user;
+
+            userQuery.Count(); // Execute the query            
+
+            foreach (var user in userQuery)
+            {
+                Console.WriteLine(user);
+            }
+        }
+        static void GroupClause()
+        {            
+
+            var userQuery =
+                from user in users
+                group user by user.Length into userGroup
+                select userGroup;
+            
+            foreach (var userGroup in userQuery)
+            {
+                Console.WriteLine($"{userGroup.Key} characters long", userGroup.Key);
+                foreach (var user in userGroup)
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# C# using LINQ Queries & Operators
+This project is a example of use of LINQ in C#.
+
+## Requirements
+- [Install](https://dotnet.microsoft.com/download/dotnet/5.0) the latest .Net 5.x SDK
+
+## Reference
+ - Course of Pluralsight of name "C#: Using LINQ Queries & Operators"
+ - [Official Documentation on Introduction to LINQ](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/introduction-to-linq-queries)
+ - [Official Documentation on Basic LINQ Query Operations](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/basic-linq-query-operations)
